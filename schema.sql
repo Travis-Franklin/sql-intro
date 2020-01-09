dbb39d87808ddf596cbc313dccf1f05b684305c0
Travis Franklin <travis.william.franklin@gmail.com>
2020-01-09 15:36:25 -0500

    adds initial files for sql database

---
+++ schema.sql
@@ -0,0 +1,7 @@
+create table albums (
+    id serial primary key,
+    title text,
+    artist text,
+    genre text
+);
+
---
+++ seed.sql
@@ -0,0 +1,7 @@
+insert into albums
+    (title, artist, genre)
+VALUES
+('kind', 'miles', 'jazz'),
+('charlie', 'vince', 'jazz'),
+('live from', 'johnny cash', 'country')
+;

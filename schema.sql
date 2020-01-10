acd54b9cc61c2d5c8a0826e1df223b03188694ae
Travis Franklin <travis.william.franklin@gmail.com>
2020-01-10 14:42:23 -0500

    adds examples of relational tables using unique indetifiers.

--- schema.sql
+++ schema.sql
@@ -1,7 +1,17 @@
+--to change a column into its own table: 
+    --put cursor above existing 'create table'
+
+create table artists (
+    id serial primary key,
+    name text
+
+);
+
 create table albums (
     id serial primary key,
     title text,
-    artist text,
-    genre text
+    -- artist text,
+    genre text,
+    artist_id integer references artists (id)
 );
 
--- seed.sql
+++ seed.sql
@@ -1,7 +1,20 @@
+
+insert into artists 
+    (name) 
+    VALUES
+        ('miles davis'),
+        ('chet baker'),
+        ('nina simone')
+        ;
+
+
 insert into albums
-    (title, artist, genre)
+    (title, genre, artist_id)
 VALUES
-('kind', 'miles', 'jazz'),
-('charlie', 'vince', 'jazz'),
-('live from', 'johnny cash', 'country')
+('kind', 'jazz', 1),
+('bitches brew', 'jazz', 1),
+('best of chet baker', 'jazz', 2),
+('nina sings the blues', 'jazz', 3)
+
 ;
+
